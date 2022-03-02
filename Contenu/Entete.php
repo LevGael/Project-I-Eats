f980fe1345fe064e035cf1c38e59da73514827e1
LevGael <99025166+LevGael@users.noreply.github.com>
2022-03-02 18:28:54 +0100

    Avancement2

--- Contenu/Entete.php
+++ Contenu/Entete.php
@@ -1,23 +1,34 @@
 <!doctype html>
 <html>
+<head>
 <link rel="stylesheet" href="../Css/Entete.css" type="text/css">
-<script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
-<div class="entete">
+<div class="entete" onclick="entete()">
 <img class="logo" src="../Images/logo.png">
-<div class="Accueil">
+<a href="Principal.php" class="lien" style="text-decoration:none;">
+<div class="Accueil" >
 <span class="iconify" data-icon="ant-design:home-filled" style="color: white; height:43px;width:43px;"></span>
 <T1>Accueil</T1>
 </div>
+</a>
+<a href="Commandes.php" class="lien" style="text-decoration:none;">
 <div class="Commandes">
 <span class="iconify" data-icon="icon-park-outline:transaction-order" style="color: white; height:43px;width:43px;"></span>
 <T1>Commandes</T1>
 </div>
+</a>
+<a href="Panier.php" class="lien" style="text-decoration:none;">
 <div class="Panier">
 <span class="iconify" data-icon="bx:bxs-cart" style="color: white; height:43px;width:43px;"></span>
 <T1>Panier</T1>
 </div>
+</a>
+<a href="Profil.php" class="lien">
 <div class="Profil">
 <span class="iconify" data-icon="ic:sharp-account-circle" style="color: white; height:43px;width:43px;"></span>
 </div>
+</a>
 </div>
+</head>
 </html>
+<script>
+</script>
--- Pages/Principal.php
+++ Pages/Principal.php
@@ -1,11 +1,41 @@
 <!doctype html>
 <html>
 <head>
+<link rel="stylesheet" href="../Css/Principal.css" type="text/css">
+<script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
 <meta charset="utf-8">
 <title>I-Eats</title>
 <?php
 include "../Contenu/Entete.php";
+
+try{
+ include '../Contenu/lire.php' ;
+} catch (PDOException $e) {
+	print("Erreur : " . $e->getMessage() . "<br/>");
+	die();
+}
+
+$sqlutil = 'SELECT * FROM restaurant';
+$prepare = $dbh->prepare($sqlutil);
+$prepare->execute();
+$restaurants = $prepare->fetchAll();
 ?>
 </head>
+<body style="margin:0px;">
+<img class="Backroundimage" width="100%" height="300" src="../Images/Backround1.png"/>
+<div class="Recherche">
+<T2>Entre ta ville et sélectionne ton restaurant</T2>
+<p></p>
+<input type="text" class="BarreRecherche">
+<input type="button" class="BouttonRecherche" onclick="Chercher" value="Rechercher"/>
+</input>
+</div>
+<?php
+foreach ($restaurants as $rests){
+?>
+<div class="BlocRestaurant"> 
 
+</div>
+<?php } ?>
+</body>
 </html>
